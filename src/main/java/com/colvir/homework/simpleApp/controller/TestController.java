a6ad214eb73e5a270adf6bf7d7a0a5a933bafe34
UriMakarov <umakarov@colvir.com>
2024-05-31 17:17:52 +0300

    add testcontroller and swagger

---
+++ src/main/java/com/colvir/homework/simpleApp/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.colvir.homework.simpleApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+    @GetMapping
+    public String hello()
+    {
+      return "hello";
+    }
+
+}
