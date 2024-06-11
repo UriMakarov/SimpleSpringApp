dd920b2d9bd2aa6a7c2046848107e92bfa2e544a
UriMakarov <umakarov@colvir.com>
2024-06-11 14:05:36 +0300

    update tests after review

--- src/test/java/com/colvir/homework/simpleApp/SimpleAppApplicationTests.java
+++ src/test/java/com/colvir/homework/simpleApp/SimpleAppApplicationTests.java
@@ -1,31 +1,13 @@
 package com.colvir.homework.simpleApp;
-
-import com.colvir.homework.simpleApp.model.Employee;
-import com.colvir.homework.simpleApp.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-class
-SimpleAppApplicationTests {
-	@Autowired
-	private EmployeeRepository employeeRepository;
+class SimpleAppApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
-	@Test
-	public void whenSaveEmployee_thenCheckFields() {
-		Employee employee = new Employee(1, "Иван", "Петров", 1000.0, "Отдел разработки");
-		Employee savedEmployee = employeeRepository.save(employee);
-		assertEquals(savedEmployee.getId(), employee.getId());
-		assertEquals(savedEmployee.getFirstName(), employee.getFirstName());
-		assertEquals(savedEmployee.getLastName(), employee.getLastName());
-		assertEquals(savedEmployee.getSalary(), employee.getSalary());
-		assertEquals(savedEmployee.getDepartment(), employee.getDepartment());
-	}
-
 }
